eb58b0230e1d9fc0924a663e3f08439541741978
FangTheWerewolf <cfls3264@gmail.com>
2024-11-10 13:09:08 -0500

    Update Test.js

--- Test.js
+++ Test.js
@@ -2,3 +2,10 @@ const myWallet = 50
 const price = 50
 
 console.log(myWallet + price)
+
+let hour = 9;
+if (hour < 12) {
+    console.log("Good morning!");
+} else {
+    console.log("Good night!");
+}
